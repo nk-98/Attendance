874d4cb67203d910ed816494324af7900a60e44f
Niko Kokko <37513526+nk-98@users.noreply.github.com>
2022-08-22 14:50:25 +0300

    Update
    Initialized NPM, added packages such as express and ejs, created a landing page(login) and a navbar.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-test
+const express = require("express"); 
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+        try {
+            res.render("landing");
+        } catch(err) {
+            console.log(err);
+        }
+})
+
+
+app.listen(3000, () => {
+    console.log("App is running...");
+})
