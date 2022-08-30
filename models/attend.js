4a7d81a7127eebc846434b4073d6d6433e8a088c
Niko Kokko <37513526+nk-98@users.noreply.github.com>
2022-08-30 14:50:35 +0300

    Add small update

--- models/attend.js
+++ models/attend.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const AttendSchema = new mongoose.Schema( {
     date: {
-        type: Date,
+        type: String,
         required: true
     },
     start: {
